75bf73bc58b622438b42d67aa1aa59f20467b20f
Sawyer Deegan <Legomaster2334@gmail.com>
2024-09-06 15:40:30 -0600

    rps begin

---
+++ scripts/rps-game.js
@@ -0,0 +1,31 @@
+//@ts-check
+
+let playerControls = document.getElementById("player-controls");
+let btnRock = document.getElementById("btn-rock");
+let btnPaper = document.getElementById("btn-paper");
+let btnScissors = document.getElementById("btn-scissors");
+
+// function rockClicked(name) {
+//     console.log("rock, was selected");
+// }
+
+// btnRock?.addEventListener("click", rockClicked);
+
+// function paperClicked(name) {
+//     console.log("paper, was selected");
+// }
+
+// btnPaper?.addEventListener("click", paperClicked);
+
+//  function scissorsClicked(name) {
+//     console.log("scissors, was selected");
+// }
+
+// btnScissors?.addEventListener("click", scissorsClicked);
+
+function playerControlHandler(e) {
+    let weaponName = e.target.innerText;
+    console.log(weaponName,  "was selected");
+}
+
+playerControls?.addEventListener("click", playerControlHandler);
