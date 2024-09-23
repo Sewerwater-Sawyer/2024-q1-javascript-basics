a2ae5fcf9c9fdd3b6717dc80d8ddb0b394a5f08d
Sawyer Deegan <Legomaster2334@gmail.com>
2024-09-23 15:45:17 -0600

    color change

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -7,9 +7,179 @@ const canvas = document.getElementById("game-canvas");
 //@ts-ignore ctx is an CanvasRenderingContext2D
 const ctx = canvas.getContext("2d");
 
-ctx.fillRect(0, 0, 50, 50);
 
-ctx.beginPath();
-ctx.arc(100, 100, 25, 0, Math.PI * 2);
-ctx.fill();
+let lastTime = 0;
+let hue = 0;
 
+function drawLoop(timestamp) {
+    let elapsedTime = timestamp - lastTime
+    lastTime = timestamp;
+
+    ctx.fillStyle = `hsla(${hue}, 100%, 50%, 100%)`;
+    ctx.beginPath();
+    ctx.arc(215, 225, 10, 0, Math.PI * 2);
+    ctx.fill();
+    hue = hue + 10;
+
+//    console.log(elapsedTime);
+
+    window.requestAnimationFrame(drawLoop);
+}
+
+window.requestAnimationFrame(drawLoop);z
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ctx.fillRect(1000, 0, 50, 50);
+
+// ctx.fillStyle = "yellow";
+// ctx.beginPath();
+// ctx.arc(400, 300, 250, 0, Math.PI * 2);
+// ctx.fill();
+// ctx.fillStyle = "00ffff";
+
+// ctx.fillStyle = "black";
+
+// ctx.beginPath();
+// ctx.arc(215, 225, 10, 0, Math.PI * 2);
+// ctx.fill();
+
+// ctx.fillStyle = "black";
+
+// ctx.beginPath();
+// ctx.arc(285, 225, 10, 0, Math.PI * 2);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.arc(250, 250, 35, 0, Math.PI);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.arc(250, 250, 60, 0, Math.PI * 2);
+// ctx.stroke();
+
+// ctx.fillRect(250, 220, 4, 45);
+// ctx.fillRect(240, 260, 10, 5);
+
+
+// ctx.beginPath();
+// ctx.arc(490, 225, 10, 0, Math.PI * 2);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.arc(560, 225, 10, 0, Math.PI * 2);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.arc(525, 250, 35, 0, Math.PI);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.arc(525, 250, 60, 0, Math.PI * 2);
+// ctx.stroke();
+
+// ctx.fillRect(525, 220, 4, 45);
+// ctx.fillRect(515, 260, 10, 5);
+
+// ctx.beginPath();
+// ctx.arc(400, 250, 240, 0, Math.PI);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.arc(1000, 300, 250, 0, Math.PI * 2);
+// ctx.stroke();
+// ctx.fillStyle = "00ffff";
+
+
+
+
+// ctx.fillStyle = "yellow";
+
+// ctx.beginPath();
+// ctx.arc(1000, 300, 250, 0, Math.PI * 2);
+// ctx.fill();
+// ctx.fillStyle = "00ffff";
+
+// ctx.fillStyle = "black";
+
+// ctx.beginPath();
+// ctx.arc(815, 225, 10, 0, Math.PI * 2);
+// ctx.fill();
+
+// ctx.fillStyle = "black";
+
+// ctx.beginPath();
+// ctx.arc(885, 225, 10, 0, Math.PI * 2);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.arc(850, 250, 35, 0, Math.PI);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.arc(850, 250, 60, 0, Math.PI * 2);
+// ctx.stroke();
+
+// ctx.fillRect(850, 220, 4, 45);
+// ctx.fillRect(840, 260, 10, 5);
+
+
+// ctx.beginPath();
+// ctx.arc(1090, 225, 10, 0, Math.PI * 2);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.arc(1160, 225, 10, 0, Math.PI * 2);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.arc(1125, 250, 35, 0, Math.PI);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.arc(1125, 250, 60, 0, Math.PI * 2);
+// ctx.stroke();
+
+// ctx.fillRect(1125, 220, 4, 45);
+// ctx.fillRect(1115, 260, 10, 5);
+
+// ctx.beginPath();
+// ctx.arc(1000, 250, 240, 0, Math.PI);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.arc(1000, 300, 250, 0, Math.PI * 2);
+// ctx.stroke();
+// ctx.fillStyle = "00ffff";
+
+// ctx.beginPath();
+// ctx.arc(1000, 250, 900, 0, Math.PI);
+// ctx.stroke();
