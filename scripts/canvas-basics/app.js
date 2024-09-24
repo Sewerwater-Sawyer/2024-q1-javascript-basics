1161e50c431b84559147e2def8cdba4f4baf22a9
Sawyer Deegan <Legomaster2334@gmail.com>
2024-09-24 16:16:25 -0600

    Moving random square

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -7,26 +7,50 @@ const canvas = document.getElementById("game-canvas");
 //@ts-ignore ctx is an CanvasRenderingContext2D
 const ctx = canvas.getContext("2d");
 
+class SquareShape {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.width = 50;
+        this.height = this.width;
+        this.hue = 0;
+        
+        this.speedMult = 11;
+        this.speedx = Math.floor(Math.random() * this.speedMult) + 1;
+        this.speedy = Math.floor(Math.random() * this.speedMult) + 1;
+
+    }
+
+    update() {
+        this.x += this.speedx;
+        this.y += this.speedy;
+        this.hue++;
+    }
+
+    draw() {
+        ctx.fillStyle = `hsla(${this.hue}, 100%, 50%, 100%)`;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+let s1 = new SquareShape(0, 0);
+
 
 let lastTime = 0;
-let hue = 0;
 
 function drawLoop(timestamp) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     let elapsedTime = timestamp - lastTime
     lastTime = timestamp;
 
-    ctx.fillStyle = `hsla(${hue}, 100%, 50%, 100%)`;
-    ctx.beginPath();
-    ctx.arc(215, 225, 10, 0, Math.PI * 2);
-    ctx.fill();
-    hue = hue + 10;
-
-//    console.log(elapsedTime);
+    s1.draw();
+    s1.update();
 
     window.requestAnimationFrame(drawLoop);
 }
 
-window.requestAnimationFrame(drawLoop);z
+window.requestAnimationFrame(drawLoop);
 
 
 
